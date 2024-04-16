0000104fa260bb140aab40c4c10afa21e641a073
vipzhicheng <vipzhicheng@users.noreply.github.com>
2024-04-16 01:31:28 +0000

    Deploying to gh-pages from @ vipzhicheng/logseq-plugin-slide-reveal@6a5c088b5535d757945a4d661cd374531844560f 🚀

---
+++ assets/changelog.md.QQLO1I8i.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as t,a5 as o}from"./chunks/framework.BQYjBzur.js";const p=JSON.parse('{"title":"Changelog","description":"","frontmatter":{},"headers":[],"relativePath":"changelog.md","filePath":"changelog.md"}'),l={name:"changelog.md"},i=o("",5),n=[i];function r(c,h,d,s,_,g){return t(),a("div",null,n)}const f=e(l,[["render",r]]);export{p as __pageData,f as default};
